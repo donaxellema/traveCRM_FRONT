670223649abf4fc2032b66d2a9d8934098472a4d
Alex <donaxellema@gmail.com>
2024-11-23 09:21:34 -0500

    Nuevo comilado
    NUeva cimpilacion

--- src/app/servicesTRAVE/global.ts
+++ src/app/servicesTRAVE/global.ts
@@ -2,8 +2,10 @@ export var global = {
     //urlImagen: 'http://localhost:3000/', //url servidor final
     //url: 'http://localhost:3000/api/', //url servidor final
 
-    urlImagen: 'http://innova-room.com/', //url DE PRODUCCION END POINTS
-    url: 'http://innova-room.com/api/', //url DE PRODUCCION DE IMAGENES
+    urlImagen: 'https://backendtravedev.shop/', //url DE PRODUCCION END POINTS
+    //urlImagen: 'https://innova-room.com/', //url DE PRODUCCION END POINTS
+    //url: 'https://innova-room.com/api/', //url DE PRODUCCION DE IMAGENES
+    url: 'https://https://backendtravedev.shop/api/', //url DE PRODUCCION DE IMAGENES
     productoSinImagen: '/assets/images/producto-sin-imagen.jpg',
     personaSinImagen: '/assets/images/persona-sin-imagen.jpg',
 }
