b4ace8bd285f351dbd2564c240870faaaf0e565e
Alex <donaxellema@gmail.com>
2024-11-23 09:31:32 -0500

    nuevo cambio
    cambio e ruta

--- src/app/servicesTRAVE/global.ts
+++ src/app/servicesTRAVE/global.ts
@@ -3,9 +3,8 @@ export var global = {
     //url: 'http://localhost:3000/api/', //url servidor final
 
     urlImagen: 'https://backendtravedev.shop/', //url DE PRODUCCION END POINTS
-    //urlImagen: 'https://innova-room.com/', //url DE PRODUCCION END POINTS
-    //url: 'https://innova-room.com/api/', //url DE PRODUCCION DE IMAGENES
-    url: 'https://https://backendtravedev.shop/api/', //url DE PRODUCCION DE IMAGENES
+    url: 'https://backendtravedev.shop/api/', //url DE PRODUCCION DE IMAGENES
+    
     productoSinImagen: '/assets/images/producto-sin-imagen.jpg',
     personaSinImagen: '/assets/images/persona-sin-imagen.jpg',
 }
